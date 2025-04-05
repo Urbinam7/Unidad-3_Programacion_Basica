2437eabea4d558334d532dc3f6d2a229fd50ca08
Urbinam7 <L24580324@reynosa.tecnm.mx>
2025-04-04 22:00:22 -0500

    Add files via upload

---
+++ Practica 1/Practica 1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Practica_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            lol = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // lol
+            // 
+            lol.AutoSize = true;
+            lol.Font = new Font("Segoe UI Black", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lol.ForeColor = SystemColors.InfoText;
+            lol.Location = new Point(159, 9);
+            lol.Name = "lol";
+            lol.Size = new Size(390, 48);
+            lol.TabIndex = 0;
+            lol.Text = "Programacion basica";
+            lol.Click += label1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.ActiveCaptionText;
+            pictureBox1.BackgroundImage = Properties.Resources.kick_buttowski;
+            pictureBox1.Image = Properties.Resources.kick_buttowski;
+            pictureBox1.Location = new Point(32, 79);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(640, 358);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.MenuHighlight;
+            ClientSize = new Size(708, 471);
+            Controls.Add(pictureBox1);
+            Controls.Add(lol);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Gif Animado";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lol;
+        private PictureBox pictureBox1;
+    }
+}
